a12d4283d8a0f3e75d5c1187e75e4566c6c36997
reeda6 <alexander.reed@vanderbilt.edu>
2019-04-04 00:44:42 -0500

    nn

--- neural_net.py
+++ neural_net.py
@@ -41,49 +41,73 @@ counter = 0
 for x in f_feats_wout_missing_data['DEPARTURE_DELAY']:
     if x == 1:
         counter = counter + 1
-        input_array.append(to_categorical(1))
+        input_array.append([1]) #to categorical?
     else:
         counter = counter + 1
-        input_array.append(to_categorical(0))
+        input_array.append([0])
 
 f_feats_wout_missing_data = np.array(f_feats_wout_missing_data.drop(columns='DEPARTURE_DELAY'))
 
 f_labels_bin = np.array(input_array)
-model = tf.keras.Sequential()
-# Adds a densely-connected layer with 64 units to the model:
-model.add(layers.Dense(64, activation='relu'))
-# Add another:
-model.add(layers.Dense(64, activation='relu'))
-# Add a softmax layer with 10 output units:
-model.add(layers.Dense(10, activation='softmax'))
 
-# Create a sigmoid layer:
-layers.Dense(64, activation='sigmoid')
-# Or:
-layers.Dense(64, activation=tf.sigmoid)
 
-# A linear layer with L1 regularization of factor 0.01 applied to the kernel matrix:
-layers.Dense(64, kernel_regularizer=tf.keras.regularizers.l1(0.01))
-
-# A linear layer with L2 regularization of factor 0.01 applied to the bias vector:
-layers.Dense(64, bias_regularizer=tf.keras.regularizers.l2(0.01))
-
-# A linear layer with a kernel initialized to a random orthogonal matrix:
-layers.Dense(64, kernel_initializer='orthogonal')
-
-# A linear layer with a bias vector initialized to 2.0s:
-layers.Dense(64, bias_initializer=tf.keras.initializers.constant(2.0))
+#model = tf.keras.Sequential()
+## Adds a densely-connected layer with 64 units to the model:
+#model.add(layers.Dense(64, activation='relu'))
+## Add another:
+#model.add(layers.Dense(64, activation='relu'))
+## Add a softmax layer with 10 output units:
+#model.add(layers.Dense(10, activation='softmax'))
+#
+## Create a sigmoid layer:
+#layers.Dense(64, activation='sigmoid')
+## Or:
+#layers.Dense(64, activation=tf.sigmoid)
+#
+## A linear layer with L1 regularization of factor 0.01 applied to the kernel matrix:
+#layers.Dense(64, kernel_regularizer=tf.keras.regularizers.l1(0.01))
+#
+## A linear layer with L2 regularization of factor 0.01 applied to the bias vector:
+#layers.Dense(64, bias_regularizer=tf.keras.regularizers.l2(0.01))
+#
+## A linear layer with a kernel initialized to a random orthogonal matrix:
+#layers.Dense(64, kernel_initializer='orthogonal')
+#
+## A linear layer with a bias vector initialized to 2.0s:
+#layers.Dense(64, bias_initializer=tf.keras.initializers.constant(2.0))
 
 model = tf.keras.Sequential([
 # Adds a densely-connected layer with 64 units to the model:
 layers.Dense(64, activation='relu', input_shape=(12,)),
 # Add another:
 layers.Dense(64, activation='relu'),
+
+layers.Dense(64, activation='relu'),
+
+layers.Dense(64, activation='relu'),
+
 # Add a softmax layer with 10 output units:
 layers.Dense(1, activation='softmax')])
+    
+#from keras import optimizers
+#sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#opt = sgd(lr=0.01)
+#model.compile(loss = "categorical_crossentropy", optimizer = opt)
 
-model.compile(optimizer=tf.train.AdamOptimizer(0.001),
-              loss='sparse_categorical_crossentropy',
+model.compile( loss='binary_crossentropy', optimizer = 'sgd',
               metrics=['accuracy'])
 
-model.fit(f_feats_wout_missing_data.data, f_labels_bin, epochs=10, batch_size=32)
+model.fit(f_feats_wout_missing_data, f_labels_bin, epochs=10, batch_size=60)
+
+
+
+
+#data = np.random.random((1000, 32))
+#labels = np.random.random((1000, 10))
+#
+#val_data = np.random.random((100, 32))
+#val_labels = np.random.random((100, 10))
+#
+#model.fit(data, labels, epochs=10, batch_size=32,
+#          validation_data=(val_data, val_labels))
+
