5a279f770849b5c61046b09f4ac261a5dc856679
Max Engel <maxengel99@gmail.com>
2019-04-14 22:14:53 -0500

    improved accuracy using more features

--- naive_bayes.py
+++ naive_bayes.py
@@ -10,7 +10,8 @@ TEST_FILE = 'Data/Test_Data.csv'
 def get_feature_values(dataset, feature):
   return list(set([row[feature] for row in dataset]))
 
-def load_data(csvfile, features=['AIRLINE', 'DAY_OF_WEEK', 'DEPARTURE_DELAY', 'DESTINATION_MIN_TEMPERATURE', 'DESTINATION_SNOW_CM', 'DISTANCE', 'MONTH', 'ORIGIN_AVG_VISIBILITY', 'ORIGIN_AVG_WIND', 'ORIGIN_MIN_TEMPERATURE', 'ORIGIN_SNOW_CM', 'SCHEDULED_DEPARTURE']):
+def load_data(csvfile, features=['AIRLINE', 'DAY_OF_WEEK', 'DEPARTURE_DELAY', "DESTINATION_AVG_VISIBILITY", 'DESTINATION_MIN_TEMPERATURE', 'DESTINATION_SNOW_CM',
+'DESTINATION_AVG_WIND', 'DISTANCE', 'MONTH', 'ORIGIN_AVG_VISIBILITY', 'ORIGIN_AVG_WIND', 'ORIGIN_MIN_TEMPERATURE', 'ORIGIN_SNOW_CM', 'SCHEDULED_DEPARTURE']):
   inputdataset = []
   unstructuredDataset = []
   with open(csvfile, newline='\n') as csvfile:
@@ -32,71 +33,104 @@ def load_data(csvfile, features=['AIRLINE', 'DAY_OF_WEEK', 'DEPARTURE_DELAY', 'D
   
   return inputdataset
 
+def create_feature_and_label_arr(data):
+  feature_values = [] #feature values
+  label_values = [] #target 
+
+  for instance in training_data:
+	  labels = list(instance.values())
+	  label_values.append(labels.pop(4))
+	  feature_values.append(labels)
+
+  return feature_values, label_values
+
+def encode_features(values, le):
+  month_arr = []
+  day_of_week_arr = []
+  airline_values_arr = []
+  scheduled_departure_arr = []
+  distance_val_arr = []
+  origin_min_temp_arr = []
+  origin_snow_arr = []
+  origin_avg_wind_arr = []
+  origin_avg_visibility_arr = []
+  dest_min_temp_arr = []
+  dest_snow_arr = []
+  dest_avg_wind_arr = []
+  dest_avg_visbility_arr = []
+
+  for instance in values:
+    month_arr.append(instance[0])
+    day_of_week_arr.append(instance[1])
+    airline_values_arr.append(instance[2])
+    scheduled_departure_arr.append(instance[3])
+    distance_val_arr.append(instance[4])
+    origin_min_temp_arr.append(instance[5])
+    origin_snow_arr.append(instance[6])
+    origin_avg_wind_arr.append(instance[7])
+    origin_avg_visibility_arr.append(instance[8])
+    dest_min_temp_arr.append(instance[9])
+    dest_snow_arr.append(instance[10])
+    dest_avg_wind_arr.append(instance[11])
+    dest_avg_visbility_arr.append(instance[12])
+
+  month_encoded = le.fit_transform(month_arr)
+  day_of_week_encoded = le.fit_transform(day_of_week_arr)
+  airline_encoded = le.fit_transform(airline_values_arr)
+  scheduled_departure_encoded = le.fit_transform(scheduled_departure_arr)
+  distance_encoded = le.fit_transform(distance_val_arr)
+  origin_min_temp_encoded = le.fit_transform(origin_min_temp_arr)
+  origin_snow_encoded = le.fit_transform(origin_snow_arr)
+  origin_avg_wind_encoded = le.fit_transform(origin_avg_wind_arr)
+  origin_avg_visibility_encoded = le.fit_transform(origin_avg_visibility_arr)
+  dest_min_temp_encoded = le.fit_transform(dest_min_temp_arr)
+  dest_snow_encoded = le.fit_transform(dest_snow_arr)
+  dest_avg_wind_encoded = le.fit_transform(dest_avg_wind_arr)
+  dest_visibility_encoded = le.fit_transform(dest_avg_visbility_arr)
+
+  features = list(zip(month_encoded, day_of_week_encoded, airline_encoded, scheduled_departure_encoded, distance_encoded, origin_min_temp_encoded,
+  origin_snow_encoded, origin_avg_wind_encoded,origin_avg_visibility_encoded, dest_min_temp_encoded, dest_snow_encoded,dest_avg_wind_encoded, dest_visibility_encoded))
+  return features
+
+'''
+Note - tried to refactor this to make it more readable - it changed the accuracy so I didn't use it
+def encode_features_and_create_test(values, le):
+  features = encode_features(values, le)
+  validation_values = []
+  for x, item in enumerate(features):
+    tmp_arr = []
+    tmp_arr.append(item[0])
+    tmp_arr.append(item[1])
+    tmp_arr.append(item[2])
+    tmp_arr.append(item[3])
+    tmp_arr.append(item[4])
+    tmp_arr.append(item[5])
+    tmp_arr.append(features[x][6])
+    tmp_arr.append(features[x][7])
+    tmp_arr.append(features[x][8])
+    tmp_arr.append(features[x][9])
+    tmp_arr.append(features[x][10])
+    tmp_arr.append(features[x][11])
+    tmp_arr.append(features[x][12])
+    validation_values.append(tmp_arr)
+
+  return validation_values
+'''
+
 training_data = load_data(TRAINING_FILE)
 validation_data = load_data(VAILDATION_FILE)
 
-feature_values = [] #feature values
-label_values = [] #target 
-
-for instance in training_data:
-	labels = list(instance.values())
-	label_values.append(labels.pop(4))
-	feature_values.append(labels)
-
+feature_values, label_values = create_feature_and_label_arr(training_data)
 le = preprocessing.LabelEncoder()
 
-month_arr = []
-day_of_week_arr = []
-airline_values_arr = []
-scheduled_departure_arr = []
-distance_val_arr = []
-origin_min_temp_arr = []
-origin_snow_arr = []
-origin_avg_wind_arr = []
-origin_avg_visibility_arr = []
-dest_min_temp_arr = []
-dest_snow_arr = []
+encoded_features = encode_features(feature_values, le)
 
-for instance in feature_values:
-  month_arr.append(instance[0])
-  day_of_week_arr.append(instance[1])
-  airline_values_arr.append(instance[2])
-  scheduled_departure_arr.append(instance[3])
-  distance_val_arr.append(instance[4])
-  origin_min_temp_arr.append(instance[5])
-  origin_snow_arr.append(instance[6])
-  origin_avg_wind_arr.append(instance[7])
-  origin_avg_visibility_arr.append(instance[8])
-  dest_min_temp_arr.append(instance[9])
-  dest_snow_arr.append(instance[10])
-
-month_encoded = le.fit_transform(month_arr)
-day_of_week_encoded = le.fit_transform(day_of_week_arr)
-airline_encoded = le.fit_transform(airline_values_arr)
-scheduled_departure_encoded = le.fit_transform(scheduled_departure_arr)
-distance_encoded = le.fit_transform(distance_val_arr)
-origin_min_temp_encoded = le.fit_transform(origin_min_temp_arr)
-origin_snow_encoded = le.fit_transform(origin_snow_arr)
-origin_avg_wind_encoded = le.fit_transform(origin_avg_wind_arr)
-origin_avg_visibility_encoded = le.fit_transform(origin_avg_visibility_arr)
-dest_min_temp_encoded = le.fit_transform(dest_min_temp_arr)
-dest_snow_encoded = le.fit_transform(dest_snow_arr)
-
-features = list(zip(month_encoded, day_of_week_encoded, airline_encoded, scheduled_departure_encoded, distance_encoded, origin_min_temp_encoded,
- origin_snow_encoded, origin_avg_wind_encoded,origin_avg_visibility_encoded, dest_min_temp_encoded, dest_snow_encoded))
-
-label_encoded = le.fit_transform(label_values)
+encoded_label = le.fit_transform(label_values)
 
 model = GaussianNB()
-model.fit(features, label_encoded)
+model.fit(encoded_features, encoded_label)
 
-feature_values = [] #feature values - validation data
-label_values = [] #target - vaidation data
-
-for instance in validation_data:
-	labels = list(instance.values())
-	label_values.append(labels.pop(4))
-	feature_values.append(labels)
+validation_feature_values, validation_label_values = create_feature_and_label_arr(validation_data)
 
 month_arr = []
 day_of_week_arr = []
@@ -109,6 +143,8 @@ origin_avg_wind_arr = []
 origin_avg_visibility_arr = []
 dest_min_temp_arr = []
 dest_snow_arr = []
+dest_avg_wind_arr = []
+dest_avg_visbility_arr = []
 
 for instance in feature_values:
   month_arr.append(instance[0])
@@ -122,6 +158,8 @@ for instance in feature_values:
   origin_avg_visibility_arr.append(instance[8])
   dest_min_temp_arr.append(instance[9])
   dest_snow_arr.append(instance[10])
+  dest_avg_wind_arr.append(instance[11])
+  dest_avg_visbility_arr.append(instance[12])
 
 month_encoded = le.fit_transform(month_arr)
 day_of_week_encoded = le.fit_transform(day_of_week_arr)
@@ -134,6 +172,8 @@ origin_avg_wind_encoded = le.fit_transform(origin_avg_wind_arr)
 origin_avg_visibility_encoded = le.fit_transform(origin_avg_visibility_arr)
 dest_min_temp_encoded = le.fit_transform(dest_min_temp_arr)
 dest_snow_encoded = le.fit_transform(dest_snow_arr)
+dest_avg_wind_encoded = le.fit_transform(dest_avg_wind_arr)
+dest_avg_visibiliy = le.fit_transform(dest_avg_visbility_arr)
 
 validation_values = []
 for x, item in enumerate(month_encoded):
@@ -149,11 +189,13 @@ for x, item in enumerate(month_encoded):
   tmp_arr.append(origin_avg_visibility_encoded[x])
   tmp_arr.append(dest_min_temp_encoded[x])
   tmp_arr.append(dest_snow_encoded[x])
+  tmp_arr.append(dest_avg_wind_encoded[x])
+  tmp_arr.append(dest_avg_visibiliy[x])
   validation_values.append(tmp_arr)
 
-
+# validation_values = encode_features_and_create_test(validation_feature_values, le)
 correct = 0
-encoded_label_values = le.fit_transform(label_values)
+encoded_label_values = le.fit_transform(validation_label_values)
 for x, item in enumerate(validation_values):
   if(model.predict([item])[0] == encoded_label_values[x]):
     correct = correct + 1
