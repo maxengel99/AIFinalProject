ec554f18c6c154fee60cc820f5aa9c807d07bc9e
reeda6 <alexander.reed@vanderbilt.edu>
2019-04-21 22:05:37 -0500

    added knn

---
+++ knn.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 21:40:27 2019
+
+@author: alexjreed7
+"""
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+
+df = pd.read_csv('./Data/added_weather_fields.csv')[['DAY_OF_WEEK', 'MONTH', 'DAY', 'DISTANCE', 'ORIGIN_AVG_VISIBILITY', 'DESTINATION_AVG_VISIBILITY', 'DESTINATION_AVG_WIND', 'ORIGIN_AVG_WIND', 'DESTINATION_SNOW_CM', 'ORIGIN_SNOW_CM', 'DESTINATION_MIN_TEMPERATURE', 'ORIGIN_MIN_TEMPERATURE', 'DEPARTURE_DELAY']].copy()
+
+f_feats_wout_missing_data = pd.DataFrame()
+for j, x in df.iterrows():
+    flag =  True
+    for y in x:
+        if not(isinstance(y, float)):
+            flag = False
+    if flag:
+        if x['DEPARTURE_DELAY'] > 15:
+            x['DEPARTURE_DELAY'] = 1
+        else:
+            x['DEPARTURE_DELAY'] = 0
+        f_feats_wout_missing_data = f_feats_wout_missing_data.append(x)
+#        print('here', x)
+
+#f_labels = f_feats_wout_missing_data[['DEPARTURE_DELAY']].copy()
+
+#f_labels_bin = df_labels['DEPARTURE_DELAY'].map({'on_time': 1, 'delayed': 0})
+
+input_array = []
+#
+counter = 0
+for x in f_feats_wout_missing_data['DEPARTURE_DELAY']:
+    if x == 1:
+        counter = counter + 1
+        input_array.append([1]) #to categorical?
+    else:
+        counter = counter + 1
+        input_array.append([0])
+        
+
+features = f_feats_wout_missing_data.drop(['DEPARTURE_DELAY'], 1)
+labels =  pd.DataFrame(input_array)
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(features, labels)
+neigh.score(features, labels)
+
+
+
+
+
--- neural_net_two.py
+++ neural_net_two.py
@@ -4,7 +4,10 @@ import numpy
 import pandas as pd
 
 #ADJUST FEATURES WE WANT TO USE UP HERE
-df = pd.read_csv('./Data/added_weather_fields.csv')[['DAY_OF_WEEK', 'MONTH', 'DAY', 'DISTANCE', 'ORIGIN_AVG_VISIBILITY', 'DESTINATION_AVG_VISIBILITY', 'DESTINATION_AVG_WIND', 'ORIGIN_AVG_WIND', 'DESTINATION_SNOW_CM', 'ORIGIN_SNOW_CM', 'DESTINATION_MIN_TEMPERATURE', 'ORIGIN_MIN_TEMPERATURE', 'DEPARTURE_DELAY']].copy()
+df = pd.read_csv('./Data/Neural_Net_Data.csv')
+df = df.reindex(columns=['AIRLINE', 'AIRLINE_DELAY', 'AIR_SYSTEM_DELAY', 'AIR_TIME', 'ARRIVAL_DELAY', 'ARRIVAL_TIME', 'CANCELLATION_REASON', 'CANCELLED', 'DAY', 'DAY_OF_WEEK', 'DEPARTURE_DELAY', 'DEPARTURE_TIME', 'DESTINATION_AIRPORT', 'DESTINATION_AVG_VISIBILITY', 'DESTINATION_AVG_WIND', 'DESTINATION_MAX_TEMPERATURE', 'DESTINATION_MIN_TEMPERATURE', 'DESTINATION_SNOW_CM', 'DISTANCE', 'DIVERTED', 'ELAPSED_TIME', 'FLIGHT_NUMBER', 'LATE_AIRCRAFT_DELAY', 'MONTH', 'ORIGIN_AIRPORT', 'ORIGIN_AVG_VISIBILITY', 'ORIGIN_AVG_WIND', 'ORIGIN_MAX_TEMPERATURE', 'ORIGIN_MIN_TEMPERATURE', 'ORIGIN_SNOW_CM', 'SCHEDULED_ARRIVAL', 'SCHEDULED_DEPARTURE', 'SCHEDULED_TIME', 'SECURITY_DELAY', 'TAIL_NUMBER', 'TAXI_IN', 'TAXI_OUT', 'WEATHER_DELAY', 'WHEELS_OFF', 'WHEELS_ON', 'YEAR'])
+df.columns = df.columns.to_series().apply(lambda x: x.strip())
+df = df[['DAY_OF_WEEK', 'MONTH', 'DAY', 'DISTANCE', 'ORIGIN_AVG_VISIBILITY', 'DESTINATION_AVG_VISIBILITY', 'DESTINATION_AVG_WIND', 'ORIGIN_AVG_WIND', 'DESTINATION_SNOW_CM', 'ORIGIN_SNOW_CM', 'DESTINATION_MIN_TEMPERATURE', 'ORIGIN_MIN_TEMPERATURE', 'DEPARTURE_DELAY']].copy()
 
 #df['DEPARTURE_DELAY_BIN'] = df['DEPARTURE_DELAY'].apply(lambda x: 0 if x <= 15 else 1)
 
@@ -20,9 +23,9 @@ for j, x in df.iterrows(): #makes sure all rows have a number (some have NaN for
         else:
             x['DEPARTURE_DELAY'] = 0
         f_feats_wout_missing_data = f_feats_wout_missing_data.append(x)
-        
-f_feats_wout_missing_data = f_feats_wout_missing_data[['DAY', 'DAY_OF_WEEK',  'DESTINATION_AVG_VISIBILITY', 'DESTINATION_AVG_WIND',
-        'DESTINATION_MIN_TEMPERATURE', 'DESTINATION_SNOW_CM', 'DISTANCE', 'MONTH', 'ORIGIN_AVG_VISIBILITY', 'ORIGIN_AVG_WIND', 'ORIGIN_MIN_TEMPERATURE', 'ORIGIN_SNOW_CM', 'DEPARTURE_DELAY']]
+#        
+#f_feats_wout_missing_data = f_feats_wout_missing_data[['DAY', 'DAY_OF_WEEK',  'DESTINATION_AVG_VISIBILITY', 'DESTINATION_AVG_WIND',
+#        'DESTINATION_MIN_TEMPERATURE', 'DESTINATION_SNOW_CM', 'DISTANCE', 'MONTH', 'ORIGIN_AVG_VISIBILITY', 'ORIGIN_AVG_WIND', 'ORIGIN_MIN_TEMPERATURE', 'ORIGIN_SNOW_CM', 'DEPARTURE_DELAY']]
 
 dataset = f_feats_wout_missing_data.values #changes pandas df to numpy matrix
 
@@ -52,8 +55,7 @@ print(len(Y_test))
 # create model
 model = Sequential()
 model.add(Dense(12, input_dim=12, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(16, init='uniform', activation='relu'))
 model.add(Dense(8, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
